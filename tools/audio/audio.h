533f92f073fb7fa2b942ab506bc7ccee48d36179
wij <41879253+wijiler@users.noreply.github.com>
2022-08-25 20:28:52 -0400

    Delete tools directory

--- tools/audio/audio.h
+++
@@ -1,5 +0,0 @@
-#include"audio.cpp"
-#define AUDIO
-#ifndef AUDIO
-int playsound (char* filename);
-#endif
--- tools/entity.h
+++
@@ -1,6 +0,0 @@
-#include"entity.cpp"
-#define ENTITY
-#ifndef ENTITY
-typedef struct Entity2d_T entity2D;
-void drawEntity ( entity2D entity,SDL_Renderer * rend,int spritewidth,int spriteheight);
-#endif
